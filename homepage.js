f4ebb933dd840f3330a98df8358f10abb24cff77
Afiq Amri <120144945+afiqamrii@users.noreply.github.com>
2024-01-13 22:55:09 +0800

    modify the homepagejs

--- homepage.js
+++ homepage.js
@@ -158,73 +158,73 @@ watchButtons.forEach(function(button) {
 var videos = [
     //0
     {
-        videoUrl: 'https://dl.dropboxusercontent.com/scl/fi/ovtzjc2kby6nh44h8yila/aquamantrailer.mp4?rlkey=3hn32ww9vm2x56pfpcq4fz0cw&dl=0',
+        videoUrl: 'images/Trailer/aquamantrailers.mp4',
         titleUrl: 'images/titles/aquamantitle.png',
         description: 'Arthur Curry, the human-born heir to the underwater kingdom of Atlantis, goes on a quest to prevent a war between the worlds of ocean and land.'
     },
     //1
     {
-        videoUrl: 'https://dl.dropboxusercontent.com/scl/fi/3t6e2xolat7ewhgknedsl/fast7trailer.mp4?rlkey=zbqgivp2278avxuo1qafht521&dl=0',
+        videoUrl: 'images/Trailer/fast7trailer.mp4',
         titleUrl: 'images/titles/fast7title.png',
         description: 'Dominic and his family are caught in a quagmire when Shaw brother seeks bloody revenge. They must not only deal with their enemy but also save a crucial programme from falling into the wrong hands.'
     },
     //2
     {
-        videoUrl: 'https://dl.dropboxusercontent.com/scl/fi/68ly7c51jc7yeu3i2lqaz/missionimposibbletrailer.mp4?rlkey=4bv9sh9or062zolhdwsjujzdt&dl=0',
+        videoUrl: 'images/Trailer/missionimposibbletrailer.mp4',
         titleUrl: 'images/titles/missionimposibbletitle.png',
         description: 'Ethan Hunt and the IMF team must track down a terrifying new weapon that threatens all of humanity if it falls into the wrong hands. With control of the future and the fate of the world at stake, a deadly race around the globe begins. Confronted by a mysterious, all-powerful enemy.'
     },
     //3
     {
-        videoUrl: 'https://dl.dropboxusercontent.com/scl/fi/vy0xq0zt14j5i6tm07x79/indianajones.mp4?rlkey=0wxyhyzjkkm5y083s2pg6fqxl&dl=0',
+        videoUrl: 'images/Trailer/indianajones.mp4',
         titleUrl: 'images/titles/indianajones.webp',
         description: 'Experience the return of legendary hero, Indiana Jones, in the fifth installment of this beloved swashbuckling series of films. Finding himself in a new era, approaching retirement, Indy wrestles with fitting into a world that seems to have outgrown him. '
     },
     //4
     {
-        videoUrl: 'https://dl.dropboxusercontent.com/scl/fi/h9ydlqo5ma41lhgsuknzs/endgame.mp4?rlkey=6iy9d7fcs56a6by17tfymbuaz&dl=0',
+        videoUrl: 'images/Trailer/endgame.mp4',
         titleUrl: 'images/titles/endgametitle.png',
         description: 'The Avengers must stop Thanos, an intergalactic warlord, from getting his hands on all the infinity stones.However, Thanos is prepared to go to any lengths to carry out his insane plan.'
     },
     //5
     {
-        videoUrl: 'https://dl.dropboxusercontent.com/scl/fi/p52g0z3ot64a4lpjb288q/homecoming.mp4?rlkey=u9qkdjxwlp4efsb51pemr681l&dl=0',
+        videoUrl: 'images/Trailer/homecoming.mp4',
         titleUrl: 'images/titles/spidermanhomecomingtitle.png',
         description: 'Peter Parker balances his life as an ordinary high school student in Queens with his superhero alter-ego Spider-Man, and finds himself on the trail of a new menace prowling the skies of New York City.'
     },
     //6
     {
-        videoUrl: 'https://dl.dropboxusercontent.com/scl/fi/dxov3idos9pu1mqfltdzi/interstellartrailer.mp4?rlkey=2tl3sg646pfeav1lfzt66ox1h&dl=0',
+        videoUrl: 'images/Trailer/interstellartrailer.mp4',
         titleUrl: 'images/titles/interstellartitle.png',
         description: 'When Earth becomes uninhabitable in the future, a farmer and ex-NASA pilot, Joseph Cooper, is tasked to pilot a spacecraft, along with a team of researchers, to find a new planet for humans.'
     },
     //7
     {
-        videoUrl: 'https://dl.dropboxusercontent.com/scl/fi/e7gidt27bzorqk5l6rrq0/parasitetrailer.mp4?rlkey=uufxqu11yb8c4ou2qa7wd05qg&dl=0',
+        videoUrl: 'images/Trailer/parasitetrailer.mp4',
         titleUrl: 'images/titles/parasitetitle.png',
         description: 'The struggling Kim family sees an opportunity when the son starts working for the wealthy Park family. Soon, all of them find a way to work within the same household and start living a parasitic life.'
     },
     //8
     {
-        videoUrl: 'https://dl.dropboxusercontent.com/scl/fi/jvpqogi351ose2a9qb4lw/starwarstrailer.mp4?rlkey=ocvpqo47fr91v6jw8kjfol15j&dl=0',
+        videoUrl: 'images/Trailer/starwarstrailer.mp4',
         titleUrl: 'images/titles/starwarstitle.png',
         description: 'A new order threatens to destroy the New Republic. Finn, Rey and Poe, backed by the Resistance and the Republic, must find a way to stop them and find Luke, the last surviving Jedi.'
     },
     //9
     {
-        videoUrl: 'https://dl.dropboxusercontent.com/scl/fi/1tfekm1o6pct6jqrtwpwu/geostormtrailer.mp4?rlkey=vjm206c5z6mpcrdgi7ja53w7x&dl=0',
+        videoUrl: 'images/Trailer/geostormtrailer.mp4',
         titleUrl: 'images/titles/geostormtitle.png',
         description: 'The Dutch Boy Program staves off natural disasters but somehow a Geostorm is approaching many countries. Jake Lawson, a technology designer, is tasked with saving the world from this disaster.'
     },
     //10
     {
-        videoUrl: 'https://dl.dropboxusercontent.com/scl/fi/z7d3fce5b9yqcoxvxpkqd/gangstercoptrailer.mp4?rlkey=zcrg59vnnqszrrh3gw833y3yt&dl=0',
+        videoUrl: 'gangstercoptrailer.mp4',
         titleUrl: 'images/titles/gangstercoptitle.png',
         description: 'A strange murder after a car accident compels a police officer to think that it is the work of a serial killer. When a gangster falls prey to a similar incident, he joins forces with the officer.'
     },
     //11
     {
-        videoUrl: 'https://dl.dropboxusercontent.com/scl/fi/9ifglaeuz8f47x1py2mra/jackreachertrailer.mp4?rlkey=8tvs04lbntoxqqi7b0iokqqfc&dl=0',
+        videoUrl: 'jackreachertrailer.mp4',
         titleUrl: 'images/titles/jackreachertitle.png',
         description: 'An ex-military police investigator sets out to prove the innocence of the head of his old unit when she is accused of treason. In his task, he discovers a major government conspiracy.'
     },
